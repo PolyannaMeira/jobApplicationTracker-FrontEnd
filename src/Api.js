98db0285853eb206582cbcc89267dbebca21b239
Etefa <etefag@gmail.com>
2024-09-15 13:29:54 +0200

    Api.js

--- src/Api.js
+++ src/Api.js
@@ -2,18 +2,14 @@
 const url = "http://localhost:5000/";
 
 const Api = {
-  getRecipes: async () => {
-    const response = await fetch(url + "recipes");
-    return response.json();
-  },
   getMyJobsList: async () => {
-    const response = await fetch(url + "myjobs");
+    const response = await fetch(url + "jobs");
     return response.json();
   },
   getMyJobsDetails: async (id) => {
-    const response = await fetch(url + `myjobs/:${id}`);
-    return response.json()
-  }
+    const response = await fetch(url + `jobs/:${id}`);
+    return response.json();
+  },
 };
 
 export default Api;
