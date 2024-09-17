3411ea1f4010af4a30bcbe0695a9901ee755b81d
polyannameira <polyannaxam@gmail.com>
2024-09-17 21:17:45 +0200

    add some css

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,32 +1,39 @@
 import  { useState } from "react";
+
 import './Filter.css'
 
 const Filter= () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [selectedFilter, setSelectedFilter] = useState("Filter"); // Valor padrão do botão
+  const [filterCriteria, setfilterCriteria] = useState("Filter"); // Valor padrão do botão
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const handleFilterClick = (filter) => {
-    setSelectedFilter(filter);
+  const handleFilterClick = (criteria) => {
+    setfilterCriteria(criteria);
     setIsMenuOpen(false);
+  };
+
+  const getCheckMark = (option, type) => {
+    if (type === 'criteria') {
+      return filterCriteria === option ? '✔️' : '';
+    }
   };
 
   return (
     <div className="filter-container">
       <button className="filter-button" onClick={toggleMenu}>
         <span className="filterImg"><img width="23" height="23" src="https://img.icons8.com/windows/32/filter.png" alt="filter"/></span>
-        <span>{selectedFilter}</span> <span className="arrow">▼</span>
+        <span>{filterCriteria}</span> <span className="arrow">▼</span>
       </button>
       {isMenuOpen && (
         <ul className="filter-menu">
           <li onClick={() => handleFilterClick("Interview Planned")}>
-            Interview Planned
+          {getCheckMark('Interview Planned', 'criteria')} Interview Planned
           </li>
           <li onClick={() => handleFilterClick("Interview Not Planned")}>
-            Interview Not Planned
+          {getCheckMark('Interview Not Planned', 'criteria')}Interview Not Planned
           </li>
           <li onClick={() => handleFilterClick("Favorites")}>Favorites</li>
         </ul>
