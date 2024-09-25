258b23b1bbb0c6e4dfdaf4c04167a327c3b555b8
Khant Maung <maungkhantdelica@gmail.com>
2024-09-25 20:03:00 +0200

    fix search bar

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,50 +1,48 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Api from "../../Api";
 
-
 const Search = () => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const [filteredData, setFilteredData] = useState([]);
-    let debounceTimer;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  let debounceTimer;
 
-    const handleInputChange = (event) => {
-        const query = event.target.value.toLowerCase();
-        setSearchQuery(query);
-        
-        if (debounceTimer) {
-            clearTimeout(debounceTimer);
-        }
+  const handleInputChange = (event) => {
+    const query = event.target.value.toLowerCase();
+    setSearchQuery(query);
 
-        debounceTimer = setTimeout(async () => {
-            if (query) {
-                const results = await Api.searchJobs(query);
-                setFilteredData(results);
-            } else {
-                setFilteredData([]); // Reset if the query is empty
-            }
-        }, 300); // Adjust the debounce time as needed
-    };
+    if (debounceTimer) {
+      clearTimeout(debounceTimer);
+    }
 
-    return (
-        <div className="search-container">
-            <input 
-                type="text"
-                placeholder="Search..."
-                value={searchQuery}
-                onChange={handleInputChange}
-                className="search-input"
-            />
-            <div className="search-results">
-                {filteredData.map((item, index) => (
-                    <div key={index} className="search-item">
-                        {item.companyName} - {item.jobRole}
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+    debounceTimer = setTimeout(async () => {
+      if (query) {
+        const results = await Api.searchJobs(query);
+        setFilteredData(results);
+      } else {
+        setFilteredData([]); // Reset if the query is empty
+      }
+    }, 300); // Adjust the debounce time as needed
+  };
+
+  return (
+    <div className="search-container">
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Search..."
+        onChange={handleInputChange}
+        value={searchQuery}
+      />
+      <span className="search-icon">🔍</span>
+      <div className="search-results">
+        {filteredData.map((item, index) => (
+          <div key={index} className="search-item">
+            {item.companyName} - {item.jobRole}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-
 export default Search;
-
